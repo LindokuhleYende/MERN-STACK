5f1da6cc2aee36d7902302f2af5051058803bcae
Lindokuhle <yendelindokuhle2@gmail.com>
2025-06-18 15:56:20 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import express from 'express'
-import cors from 'cors'
+import cors from 'cors' //cross origin resourse sharing =>ensures that our backend is shared across any domains
 import movies from './api/movies.route.js'
 
 const app = express() //create the server
 
-// Attach the cors and express.json middleware that express will use
-
+//Middleware configuration
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) //nsures the server can parse incoming JSON request bodies
 
+//Every route will start with this url
 app.use("/api/v1/movies", movies)
-app.use('*', (req,res)=>{
-res.status(404).json({error: "not found"})
+app.use('*', (req, res) => {
+    res.status(404).json({ error: "not found" })
 })
 export default app
