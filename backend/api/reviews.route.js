451f94a055ed95f3e50492fef997061b96592792
Lindokuhle <yendelindokuhle2@gmail.com>
2025-06-19 11:36:04 +0200

    Update reviews.route.js

--- backend/api/reviews.route.js
+++ backend/api/reviews.route.js
@@ -3,11 +3,8 @@ import ReviewsController from './reviews.controller.js'
 
 const router = express.Router()
 
-// Get all reviews for a specific movie
-router.route('/movie/:movieId').get(ReviewsController.apiGetReviewsByMovieId)
-
-// Create a review
-router.route('/')
+router
+    .route('/')
     .post(ReviewsController.apiPostReview)
     .put(ReviewsController.apiUpdateReview)
     .delete(ReviewsController.apiDeleteReview)
