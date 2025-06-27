1756160e0d89f0983999de4552e5f22622eeb7d0
Lindokuhle <yendelindokuhle2@gmail.com>
2025-06-27 11:53:00 +0200

    Create greetingBanner.js

---
+++ front-end/src/components/greetingBanner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GreetingBanner = ({ name }) => {
+    return (
+        <div>
+            <h5>
+                Welcome {name}
+            </h5>
+        </div>
+    )
+}
+export default GreetingBanner
