bb8b9c22e84ba79f4d75e094a49311f2fff64814
Lindokuhle <yendelindokuhle2@gmail.com>
2025-07-03 10:09:41 +0200

    Update movies.js

--- front-end/src/services/movies.js
+++ front-end/src/services/movies.js
@@ -2,36 +2,36 @@ import axios from "axios";
 
 class MovieDataService {
     getAll(page = 0) { //Fetches all movies with optional pagination
-        return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`)
+        return axios.get(`https://mern-stack-backend-9v4j.onrender.com/api/v1/movies?page=${page}`)
     }
     get(id) {
-        return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
+        return axios.get(`https://mern-stack-backend-9v4j.onrender.com/api/v1/movies/id/${id}`)
     }
     // find(title) {
     //     return axios.get(
-    //         `http://localhost:5000/api/v1/movies?title=${title}`
+    //         `https://mern-stack-backend-9v4j.onrender.com/api/v1/movies?title=${title}`
     //     )
     // }
     find(query, by = "title", page = 0, rating) {//Searches for movies dynamically by a field
         return axios.get(
-            `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
+            `https://mern-stack-backend-9v4j.onrender.com/api/v1/movies?${by}=${query}&page=${page}&rating=${rating}`
         )
     }
     createReview(data) {//Sends a new review via POST.
-        return axios.post("http://localhost:5000/api/v1/movies/review", data)
+        return axios.post("https://mern-stack-backend-9v4j.onrender.com/api/v1/movies/review", data)
     }
     updateReview(data) {//Updates an existing review via PUT.
-        return axios.put("http://localhost:5000/api/v1/movies/review", data)
+        return axios.put("https://mern-stack-backend-9v4j.onrender.com//api/v1/movies/review", data)
     }
     deleteReview(id, userId) {//Sends a DELETE request, with both review and user IDs passed in the request body.
         return axios.delete(
-            "http://localhost:5000/api/v1/movies/review",
+            "https://mern-stack-backend-9v4j.onrender.com/api/v1/movies/review",
             { data: { review_id: id, user_id: userId } }
         )
     }
     getRatings() {//- Fetches a list of ratings (possibly for filters or display).
 
-        return axios.get("http://localhost:5000/api/v1/movies/ratings")
+        return axios.get("https://mern-stack-backend-9v4j.onrender.com/api/v1/movies/ratings")
     }
 }
 
